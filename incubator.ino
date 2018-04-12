62e2af87c00e7146ff6d40f2f687ab229efe8b82
Daniel Wu <dw352934@gmail.com>
2018-04-12 17:27:07 -0400

    Added switch cases

--- incubator.ino
+++ incubator.ino
@@ -1,23 +1,43 @@
-int led = 2;           // the PWM pin the LED is attached to
 int heater = 4;
-
+int pinA0 = pinA0;
+int current = 0;
+int adc;
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  pinMode(pinA0, INPUT);
   pinMode(heater, OUTPUT);
+  current = 1;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
-  digitalWrite(led, HIGH);
-  digitalWrite(heater, HIGH);
- 
-  delay(10000);
-  
-  digitalWrite(led, LOW);
-  digitalWrite(heater, LOW);
-  
-  delay(10000);
+  /* Switches cases are from 1-4. 1- Default(Start-Up/Set-up, Will likely include calibration) 2- Heating 3- Cooling 4- Finished */
+  if(current>=1 and current<=4)
+  {
+    adc = analogRead(pinA0);
+    Serial.println(adc);
+    switch(current)
+    {
+      case 1:
+        Serial.println('1');
+         current = 2;
+         break;
+      case 2:
+        Serial.println('2');
+        current = 3;
+        break;
+      case 3:
+        Serial.println('3');
+        current = 4;
+        break;
+      case 4:
+        Serial.println('4');
+        current = 1; /* This is only here for now because I'm not sure what we're going to do with it */
+        break;
+    }
+  }
+  Serial.println("Here");
+  delay(1000);
 }
